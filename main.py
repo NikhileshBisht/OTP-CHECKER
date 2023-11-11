a459480f39aa14813230716f7a316b899c29c66b
NIKHILESH BISHT <96682240+hotshotcs@users.noreply.github.com>
2023-11-11 11:56:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import random
 
 n = random.randint(1000,9999)
 
-account_sid = 'AC8855bd4f6b28926dbf05736df367ba12'
-auth_token = '54643209f5917a9763d9b8f6af4e0e3a'
+account_sid = 'AC8855bd4f6b2892***********2'
+auth_token = '54643209f5917a97***********'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-  from_='+12562900939',
+  from_='+1256290***',
   body='Your OTP For Login is :' + str(n),
-  to='+919971514995'
+  to='+9199715*****'
 )
 
 while(True):
